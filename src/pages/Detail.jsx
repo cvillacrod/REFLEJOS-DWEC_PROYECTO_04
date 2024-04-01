8cb69cce62e368797a76922ba05b3b7850a1f5a6
Cristina <cvillacrod@educacion.navarra.es>
2024-04-02 00:19:28 +0200

    resultado v1

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,11 +1,37 @@
 import React, { useEffect, useState } from 'react';
 //importando los modulos de firebase
 import { doc, getDoc } from "firebase/firestore";
-import { db } from "./../firebase/credentials";
+import { db } from "../firebase/credentials";
 import { useParams } from 'react-router-dom';
 import PrimarySearchAppBar from './Toolbar';
 import { hydrate } from './helper';
 import { cloneDeep } from 'lodash'
+import PublishedWithChangesTwoToneIcon from '@mui/icons-material/PublishedWithChangesTwoTone';
+import HourglassBottomOutlinedIcon from '@mui/icons-material/HourglassBottomOutlined';
+import WarningIcon from '@mui/icons-material/Warning';
+import HistoryTwoToneIcon from '@mui/icons-material/HistoryTwoTone';
+import HikingTwoToneIcon from '@mui/icons-material/HikingTwoTone';
+import Timeline from '@mui/lab/Timeline';
+import TimelineItem from '@mui/lab/TimelineItem';
+import TimelineSeparator from '@mui/lab/TimelineSeparator';
+import TimelineConnector from '@mui/lab/TimelineConnector';
+import AddTaskTwoToneIcon from '@mui/icons-material/AddTaskTwoTone';
+import TimelineContent from '@mui/lab/TimelineContent';
+import TimelineDot from '@mui/lab/TimelineDot';
+import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import { grey } from '@mui/material/colors';
+import { blue } from '@mui/material/colors';
+import Icon from '@mui/material/Icon';
+
+/*para la fecha y hora*/
+import dayjs from 'dayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DemoContainer, DemoItem } from '@mui/x-date-pickers/internals/demo';
+import { DesktopDateTimePicker } from '@mui/x-date-pickers/DesktopDateTimePicker';
 
 export default function Detail() {
 
@@ -82,22 +108,208 @@ export default function Detail() {
         resultados.map((res, i) => (
           <div key={i} className="max-w-sm rounded overflow-hidden shadow-lg mx-2 ">
             <div className="px-6 py-4">
-              Programa: {res.idprograma.descripcion}
+              Programa: {res.idprograma.descripcion}<br />
+              Tiempo ejercicio: {res.idprograma.tejercicio}<br />
+              Tiempo descanso: {res.idprograma.tejercicio}<br />
+              Distancia: {res.idprograma.tejercicio}<br />
+              Numero de ciclos: {res.idprograma.nciclos}<br />
               <br />
               -----------------------------------
               <br />
-              Fecha: {Date(res?.fecha)} <br />
-              Distancia dispositivo: {res?.distanciaaldispositivo} <br />
-              Media tiempo reaccion : {res?.mediatiemporeaccion}<br />
-              Numero de dispositivos apagados {res?.numerodispositivosapagados}<br />
-              ...
+              <strong>Fecha:</strong>
 
+              <LocalizationProvider dateAdapter={AdapterDayjs}>   
+
+                  <DemoContainer
+                    components={[
+                      'DateTimePicker',
+                      'MobileDateTimePicker',
+                      'DesktopDateTimePicker',
+                      'StaticDateTimePicker',
+                    ]}
+                  >
+                    <DemoItem>
+                      <DesktopDateTimePicker
+                          readOnly  
+                          format="DD-MMMM-YYYY HH:mm"
+                          // Utiliza el valor de fecha completa res?.fecha como defaultValue
+                          defaultValue={res?.fecha && dayjs(res?.fecha.seconds * 1000)}
+                        />
+                   </DemoItem>                  
+                
+                  </DemoContainer>
+                </LocalizationProvider>
+
+
+
+              <Timeline position="alternate">
+
+                <TimelineItem>
+                  <TimelineOppositeContent
+                    sx={{ m: 'auto 0' }}
+                    align="right"
+                    variant="body2"
+                    color="text.secondary"
+                  >
+                    {res?.distanciaaldispositivo}
+                  </TimelineOppositeContent>
+                  <TimelineSeparator>
+                    <TimelineConnector />
+                    <TimelineDot>                      
+                      <HikingTwoToneIcon/>
+                      <RepeatIcon />
+                    </TimelineDot>
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <TimelineContent sx={{ py: '12px', px: 2 }}>
+                    <Typography variant="h6" component="span">
+                    Distancia
+                    </Typography>
+                    <Typography variant="body2">dispositivo</Typography>
+                  </TimelineContent>
+                </TimelineItem>
+
+                <TimelineItem>
+                  <TimelineOppositeContent
+                    sx={{ m: 'auto 0' }}
+                    variant="body2"
+                    color="text.secondary"
+                  >
+                    {res?.numerodispositivosapagados} dispositivos
+                  </TimelineOppositeContent>
+
+                  <TimelineSeparator>
+                    <TimelineConnector />
+                    <TimelineDot color="success">     
+                      <AddTaskTwoToneIcon sx={{fontSize: 30 }}/>
+                    </TimelineDot>
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <TimelineContent sx={{ py: '12px', px: 2 }}>
+                    <Typography variant="h6" component="span">
+                      Apagados
+                    </Typography>
+                    <Typography variant="body2">cantidad correctos</Typography>
+                  </TimelineContent>
+                </TimelineItem>
+
+                <TimelineItem>
+                <TimelineOppositeContent
+                    sx={{ m: 'auto 0' }}
+                    align="right"
+                    variant="body2"
+                    color="text.secondary"
+                  >
+                    {res?.numerofallos}
+                  </TimelineOppositeContent>
+                  <TimelineSeparator>
+                    <TimelineConnector />
+                    <TimelineDot variant="primary" sx={{ color: red[500] }}>     
+                      <WarningIcon sx={{fontSize: 30 }}/>
+                    </TimelineDot>
+       
+                    <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+                  </TimelineSeparator>
+                  <TimelineContent sx={{ py: '12px', px: 2 }}>
+                    <Typography variant="h6" component="span">
+                      Fallos
+                    </Typography>
+                  </TimelineContent>
+                </TimelineItem>
+
+                <TimelineItem>
+                <TimelineOppositeContent
+                    sx={{ m: 'auto 0' }}
+                    align="right"
+                    variant="body2"
+                    color="text.secondary"
+                  >
+                    {res?.tiempototalempleado}
+                  </TimelineOppositeContent>
+                  <TimelineSeparator>
+                    <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+                    <TimelineDot  variant="sharp" sx={{ color: blue[500] }}>
+                      <PublishedWithChangesTwoToneIcon sx={{ color: blue[500] , fontSize: 30 }}/>
+                    </TimelineDot>
+
+
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <TimelineContent sx={{ py: '12px', px: 2 }}>
+                    <Typography variant="h6" component="span">
+                    Reaccion
+                    </Typography>
+                    <Typography variant="body2">media de tiempo </Typography>
+                  </TimelineContent>
+                </TimelineItem>
+
+                <TimelineItem>
+                <TimelineOppositeContent
+                    sx={{ m: 'auto 0' }}
+                    align="right"
+                    variant="body2"
+                    color="text.secondary"
+                  >
+                    {res?.tiempototalejercicio}
+                  </TimelineOppositeContent>
+                  <TimelineSeparator>
+                    <TimelineConnector />
+                    
+                    <TimelineDot color="secondary">                     
+                     <HistoryTwoToneIcon sx={{ fontSize: 30 }}/>
+                    </TimelineDot>
+
+                    <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+                  </TimelineSeparator>
+                  <TimelineContent sx={{ py: '12px', px: 2 }}>
+                    <Typography variant="h6" component="span">
+                      Total
+                    </Typography>
+                    <Typography variant="body2">tiempo del ejercicio</Typography>
+                  </TimelineContent>
+                </TimelineItem>
+
+                <TimelineItem>                
+                <TimelineOppositeContent
+                    sx={{ m: 'auto 0' }}
+                    align="right"
+                    variant="body2"
+                    color="text.secondary"
+                  >
+                    {res?.tiempototalempleado}
+                  </TimelineOppositeContent>
+                  <TimelineSeparator>
+                    <TimelineConnector sx={{ bgcolor: 'secondary.main' }} />
+                             
+                    <TimelineDot variant="primary" sx={{ color: grey[700] }}>     
+                      <HourglassBottomOutlinedIcon sx={{fontSize: 30 }}/>
+                    </TimelineDot>
+
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <TimelineContent sx={{ py: '12px', px: 2 }}>
+                    <Typography variant="h6" component="span">
+                      Ejecucion
+                    </Typography>
+                    <Typography variant="body2">tiempo total empleado</Typography>
+                  </TimelineContent>
+                </TimelineItem>
+                
+              </Timeline>
+
+    
             </div>
-          </div>
+
+               </div>
         ))
       ) : (
         <p>Cargando datos...</p>
       )}
+
+
+      
     </div>
+
+    
   </div>;
 }
